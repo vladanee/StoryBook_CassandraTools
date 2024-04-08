1c1218bd056e3fbd4a45c54daebb2cdc3a18ad63
vladanee <vlacvetkov@gmail.com>
2024-04-08 16:43:53 +0200

    Search

---
+++ src/stories/Search/CustomPagination.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { usePagination } from 'react-instantsearch-hooks-web'
+import Pagination from '../UI/Pagination/Pagination'
+
+function CustomPagination(props) {
+  const { currentRefinement, nbPages, refine } = usePagination(props)
+
+  return <Pagination currentRefinement={currentRefinement} nbPages={nbPages} refine={refine} />
+}
+
+export default CustomPagination
